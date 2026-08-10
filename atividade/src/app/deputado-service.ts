758f2a979747783100ce3e3719ee9e367ee5732e
ludmila tawane rodrigues dos anjos <ludmia.tawane@escolar.ifrn.edu.br>
2026-08-10 08:08:28 -0300

    jtjt

--- atividade/src/app/deputado-service.ts
+++ atividade/src/app/deputado-service.ts
@@ -1,4 +1,12 @@
-import { Service } from '@angular/core';
+import { inject, Service } from '@angular/core';
 
 @Service()
-export class DeputadoService {}
+export class DeputadoService {
+  readonly url = 'https://dadosabertos.camara.leg.br/api/v2';
+   readonly #http = inject(HttpClient);
+  obterTodos(){
+    return this.#http.get('${this.API}/deputados?ordem=ASC&ordenarPor=nome')
+    ;
+
+  }
+}
