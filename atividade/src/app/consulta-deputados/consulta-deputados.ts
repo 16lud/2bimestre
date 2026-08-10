3c34f83a3f839a3bed1047059b6927d2209cc07f
ludmila tawane rodrigues dos anjos <ludmia.tawane@escolar.ifrn.edu.br>
2026-08-10 07:53:21 -0300

    gdf

---
+++ atividade/src/app/consulta-deputados/consulta-deputados.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta-deputados',
+  imports: [],
+  templateUrl: './consulta-deputados.html',
+  styleUrl: './consulta-deputados.css',
+})
+export class ConsultaDeputados {}
