44b837b9f4ace905f24cac5336846b937e78d2a4
ludmila tawane rodrigues dos anjos <ludmia.tawane@escolar.ifrn.edu.br>
2026-08-11 11:18:06 -0300

    hktdktd

--- atividade/src/app/consulta-deputados/consulta-deputados.ts
+++ atividade/src/app/consulta-deputados/consulta-deputados.ts
@@ -1,76 +1,42 @@
-import { Component, inject, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, signal, OnInit } from '@angular/core';
 import { DeputadoService } from '../deputado-service';
-import { Lider } from '../deputado';
+import { Deputado } from '../deputado';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-consulta-deputados',
-  imports: [FormsModule],
+  imports: [NgOptimizedImage],
   templateUrl: './consulta-deputados.html',
   styleUrls: ['./consulta-deputados.css'],
 })
-export class ConsultaDeputados {
+export class ConsultaDeputados implements OnInit {
   readonly #deputadoService = inject(DeputadoService);
-  protected lideres = signal<Lider[] | undefined>(undefined);
-  protected idPartido = signal<number | null>(null);
-  protected dataInicio = signal<string>('');
-  protected dataFim = signal<string>('');
+  protected deputados = signal<Deputado[] | undefined>(undefined);
+  protected carregando = signal<boolean>(true);
   protected erro = signal<string>('');
-  protected carregando = signal<boolean>(false);
 
-  buscarLideres(): void {
-    if (!this.idPartido()) {
-      this.erro.set('Por favor, informe o ID do partido');
-      return;
-    }
+  ngOnInit(): void {
+    this.carregarDeputados();
+  }
 
+  carregarDeputados(): void {
     this.carregando.set(true);
     this.erro.set('');
 
-    this.#deputadoService
-      .obterLideresPorPartido(
-        this.idPartido()!,
-        this.dataInicio() || undefined,
-        this.dataFim() || undefined
-      )
-      .subscribe({
-        next: (res) => {
-          this.lideres.set(res.dados);
-          this.carregando.set(false);
-        },
-        error: (err) => {
-          this.erro.set('Erro ao buscar líderes. Verifique o ID do partido.');
-          this.carregando.set(false);
-          console.error(err);
-        },
-      });
+    this.#deputadoService.obterTodos().subscribe({
+      next: (res) => {
+        this.deputados.set(res.dados);
+        this.carregando.set(false);
+      },
+      error: (err) => {
+        this.erro.set('Erro ao carregar deputados.');
+        this.carregando.set(false);
+        console.error(err);
+      },
+    });
   }
 
-  buscarTodosLideres(): void {
-    this.carregando.set(true);
-    this.erro.set('');
-
-    this.#deputadoService
-      .obterTodosLideres(
-        this.dataInicio() || undefined,
-        this.dataFim() || undefined
-      )
-      .subscribe({
-        next: (res) => {
-          this.lideres.set(res.dados);
-          this.carregando.set(false);
-        },
-        error: (err) => {
-          this.erro.set('Erro ao buscar líderes.');
-          this.carregando.set(false);
-          console.error(err);
-        },
-      });
-  }
-
-  formatarData(data: string | null): string {
-    if (!data) return 'Indefinido';
-    const date = new Date(data);
-    return date.toLocaleDateString('pt-BR');
+  obterAnoCandidatura(idLegislatura: number): number {
+    return 2023 + (idLegislatura - 56) * 4;
   }
 }
